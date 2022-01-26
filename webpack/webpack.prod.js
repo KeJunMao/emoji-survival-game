64a08aabaec56e6c0fb515c957f492a0786acc34
KeJun <kejun1997@gmail.com>
2022-01-26 17:45:12 +0800

    chore(src): 混淆代码

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,7 +2,7 @@ const path = require('path')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 const { InjectManifest } = require('workbox-webpack-plugin')
-// const WebpackObfuscator = require('webpack-obfuscator')
+const WebpackObfuscator = require('webpack-obfuscator')
 
 const prod = {
   mode: 'production',
@@ -22,14 +22,14 @@ const prod = {
   },
   plugins: [
     // disabled by default (uncomment to active)
-    // new WebpackObfuscator(
-    //   {
-    //     rotateStringArray: true,
-    //     stringArray: true,
-    //     stringArrayThreshold: 0.75
-    //   },
-    //   ['vendors.*.js', 'sw.js']
-    // ),
+    new WebpackObfuscator(
+      {
+        rotateStringArray: true,
+        stringArray: true,
+        stringArrayThreshold: 0.75
+      },
+      ['vendors.*.js', 'sw.js']
+    ),
     new InjectManifest({
       swSrc: path.resolve(__dirname, '../pwa/sw.js'),
       swDest: 'sw.js'
